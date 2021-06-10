389004531527f07c86c5949679e94a4201e7889a
AK-SQUAREOPS <84565898+AK-SQUAREOPS@users.noreply.github.com>
2021-06-10 11:32:35 +0530

    Update appstop.sh

--- appstop.sh
+++ appstop.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#pm2 stop /home/ubuntu/node-express-realworld-example-app/app.js
- cd /home/ubuntu/node-express-realworld-example-app
- npm stop
+cd /home/ubuntu/node-express-realworld-example-app
+npm stop
