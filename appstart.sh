49daaf47742716c6054c84c08ca4cacbc17c4b01
AK-SQUAREOPS <84565898+AK-SQUAREOPS@users.noreply.github.com>
2021-06-10 11:27:18 +0530

    Update appstart.sh

--- appstart.sh
+++ appstart.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
- pm2 start /home/ubuntu/node-express-realworld-example-app/app.js -i 0 --name "app"
+ #pm2 start /home/ubuntu/node-express-realworld-example-app/app.js -i 0 --name "app"
+ cd /home/ubuntu/node-express-realworld-example-app
+ npm start
